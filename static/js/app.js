ecaa40f0f8fb071b7f24aeb0121a198ccfc478f7
eddie0rNot <eddie.buckalew@gmail.com>
2021-04-11 09:30:43 -0500

    update js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,32 @@
-var data = "static/js/data/samples.json"
+var sampleData = "static/js/data/samples.json";
 
-d3.json(data,function(sampleData) {
+function unpack(rows,index) {
+  return rows.map(function(row){
+    return row[index];
+  });
+}
+
+d3.json(data).then(function(samples){
+  var data = samples.samples[0];
     console.log(data);
-  });  
+  
+  data.sort(function(a,b){
+    return parseFloat(b.otu_ids) - parseFloat(a.otu_ids);
+
+  });
+
+data = data.slice(0,10);
+
+data = data.reverse();
+
+var trace1 = {
+  type: "bar",
+  x: data.map(row => row.sample_values),
+  y: data.map(row => row.otu_ids),
+  orientation: 'h'
+};
+
+var chartData = [trace1];
+
+Plotly.newPlot("bar",chartData);
+});
